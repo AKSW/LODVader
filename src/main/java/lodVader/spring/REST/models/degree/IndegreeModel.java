588f291bbe2f3d218f824f2b67cbfb1627a30ab3
cirola2000 <cirobaroneto@gmail.com>
2016-01-15 15:44:17 +0100

    mapReduce added for degrees

--- src/main/java/lodVader/spring/REST/models/degree/IndegreeModel.java
+++ src/main/java/lodVader/spring/REST/models/degree/IndegreeModel.java
@@ -7,13 +7,17 @@ import com.mongodb.DBObject;
 import com.mongodb.MapReduceCommand;
 
 import lodVader.mongodb.DBSuperClass2;
+import lodVader.mongodb.collections.DatasetDB;
 import lodVader.mongodb.collections.DistributionDB;
 import lodVader.mongodb.collections.LinksetDB;
+import lodVader.mongodb.collections.ResourceDB;
 import lodVader.mongodb.collections.TmpMapReduce;
 
 public class IndegreeModel {
 
 	public StringBuilder result = new StringBuilder();
+
+	public boolean isDataset = false;
 
 	/**
 	 * MapReduce functions for indegree linksets
@@ -49,15 +53,14 @@ public class IndegreeModel {
 		result.append("\n\n\n\n===============================================================\n");
 		result.append("Comparing with vocabularies\n");
 		result.append("===============================================================\n\n");
-		
-		
+
 		result.append("=====================================================\n");
 		result.append("Top " + n + " by number of indegree links\n");
 		result.append("=====================================================\n\n");
 
 		printTableIndegree(instances);
 
-		instances = collection.find().sort(new BasicDBObject("value.totalDistributionsOutdegree", -1)).limit(n);
+		instances = collection.find().sort(new BasicDBObject("value.totalIndegree", -1)).limit(n);
 
 		result.append("=====================================================\n");
 		result.append("Top " + n + " by number of indegree distributions/datasets\n");
@@ -76,7 +79,7 @@ public class IndegreeModel {
 
 		collection = DBSuperClass2.getDBInstance().getCollection(TmpMapReduce.COLLECTION_NAME);
 		instances = collection.find().sort(new BasicDBObject("value.links", -1)).limit(n);
-		
+
 		result.append("\n\n\n\n===============================================================\n");
 		result.append("Comparing with datasets/distributions\n");
 		result.append("===============================================================\n\n");
@@ -87,7 +90,7 @@ public class IndegreeModel {
 
 		printTableIndegree(instances);
 
-		instances = collection.find().sort(new BasicDBObject("value.totalDistributionsOutdegree", -1)).limit(n);
+		instances = collection.find().sort(new BasicDBObject("value.totalIndegree", -1)).limit(n);
 
 		result.append("=====================================================\n");
 		result.append("Top " + n + " by number of indegree distributions\n");
@@ -101,15 +104,19 @@ public class IndegreeModel {
 		result.append("Name\t Indegree \t Links\n");
 
 		BasicDBObject values = null;
-		DistributionDB tmpDistribution;
+		ResourceDB tmpDistribution;
 
 		for (DBObject instance : instances) {
 
-			tmpDistribution = new DistributionDB(((Number) (instance.get("_id"))).intValue());
+			if (!isDataset)
+				tmpDistribution = new DistributionDB(((Number) (instance.get("_id"))).intValue());
+			else
+				tmpDistribution = new DatasetDB(((Number) (instance.get("_id"))).intValue());
+
 			values = new BasicDBObject((BasicDBObject) instance.get("value"));
 
 			result.append(tmpDistribution.getTitle());
-			result.append("\t" + values.get("totalDistributionsIndegree"));
+			result.append("\t" + values.get("totalIndegree"));
 			result.append("\t" + values.getString("links"));
 
 			result.append("\n");
