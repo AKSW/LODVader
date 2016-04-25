4547b905232e064c8a07151947de162df8189ef1
cirola2000 <cirobaroneto@gmail.com>
2016-04-25 13:21:22 +0200

    -

--- src/main/java/lodVader/spring/REST/controllers/CouplingCohesionController.java
+++ src/main/java/lodVader/spring/REST/controllers/CouplingCohesionController.java
@@ -6,36 +6,35 @@ import lodVader.mongodb.collections.DistributionDB;
 import lodVader.mongodb.queries.DatasetQueries;
 import lodVader.mongodb.queries.DistributionQueries;
 
-//@RestController
 public class CouplingCohesionController {
 
 	// @RequestMapping(value = "/CouplingCohesion/Dataset", method =
 	// RequestMethod.GET)
-	public static void main(String[] args) {
-
-		// }
-		// public void load() {
-		for (DatasetDB dataset : new DatasetQueries().getDatasets(false)) {
-
-			int totalTriples = 0;
-			int totalCohesion = 0;
-			int totalObjectResources = 0;
-
-			for (DistributionDB distribution : new DistributionQueries().getDistributions(false,
-					DatasetStatus.DONE.toString(), dataset.getLODVaderID())) {
-				totalTriples = totalTriples + distribution.getTriples();
-				totalCohesion = totalCohesion + distribution.getObjectCohesion();
-				totalObjectResources = totalObjectResources + distribution.getNumberOfObjectTriples();
-			}
-
-			if (totalTriples > 0) {
-				System.out.println("Dataset: " + dataset.getTitle());
-				System.out.println("Total Triples: " + totalTriples);
-				System.out.println("Total Cohesion Links: " + totalCohesion);
-				System.out.println("Cohesion: " + new Double(totalCohesion/totalTriples));
-				System.out.println("Total Object Resources: " + totalObjectResources);
-				System.out.println();
-			}
-		}
-	}
+//	public static void main(String[] args) {
+//
+//		// }
+//		// public void load() {
+//		for (DatasetDB dataset : new DatasetQueries().getDatasets(false)) {
+//
+//			int totalTriples = 0;
+//			int totalCohesion = 0;
+//			int totalObjectResources = 0;
+//
+//			for (DistributionDB distribution : new DistributionQueries().getDistributions(false,
+//					DatasetStatus.DONE.toString(), dataset.getLODVaderID())) {
+//				totalTriples = totalTriples + distribution.getTriples();
+//				totalCohesion = totalCohesion + distribution.getObjectCohesion();
+//				totalObjectResources = totalObjectResources + distribution.getNumberOfObjectTriples();
+//			}
+//
+//			if (totalTriples > 0) {
+//				System.out.println("Dataset: " + dataset.getTitle());
+//				System.out.println("Total Triples: " + totalTriples);
+//				System.out.println("Total Cohesion Links: " + totalCohesion);
+//				System.out.println("Cohesion: " + new Double(totalCohesion/totalTriples));
+//				System.out.println("Total Object Resources: " + totalObjectResources);
+//				System.out.println();
+//			}
+//		}
+//	}
 }
